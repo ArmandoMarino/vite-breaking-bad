fd89e46236f39d3dc01b04236485eed8b9fba02e
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-24 16:00:39 +0100

    change logo img and begin AppMain

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@ export default {
 
 <template>
   <header>
-
+    <img src="./assets/img/pk-logo.png" alt="">
   </header>
 </template>
 
 <style lang="scss">
-@use './assets/scss/style.scss'
+@use './assets/scss/style.scss';
 </style>
