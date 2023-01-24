656dbd614f42577add097dcc981f0ed1fa9a6a73
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-24 18:13:04 +0100

    some css

--- src/components/pokemons/PokemonCard.vue
+++ src/components/pokemons/PokemonCard.vue
@@ -11,14 +11,28 @@ export default {
 </script>
 
 <template>
-    <div class="pokemon-card text-center p-2">
+    <div class="pokemon-card text-center p-3">
         <img class="img-fluid mb-3" :src="image" :alt="name">
         <p>#{{ number }}</p>
-        <p>{{ name }}</p>
+        <p><strong>{{ name }} </strong> </p>
         <p>{{ type }}</p>
     </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as*;
 
+.pokemon-card {
+    background-color: white;
+    border-radius: 50px;
+
+    p {
+        font-style: italic;
+    }
+
+    img {
+        width: 200px;
+        height: 200px;
+    }
+}
 </style>
--- src/components/pokemons/PokemonList.vue
+++ src/components/pokemons/PokemonList.vue
@@ -14,13 +14,16 @@ export default {
 
 <template>
     <section id="pokemons">
-        <div class="row g-4 row-cols-2 row-cols-md-3 row-cols-lg-4">
+        <div class="row mt-2 g-4 row-cols-2 row-cols-md-3 row-cols-lg-4">
             <pokemon-card v-for="pokemon in store.pokemons" :key="pokemon.id" :number="pokemon.number"
                 :name="pokemon.name" :image="pokemon.imageUrl" :type="pokemon.type1"></pokemon-card>
         </div>
     </section>
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+#pokemons {
+    height: 0vh;
+    overflow-y: auto;
+}
 </style>
