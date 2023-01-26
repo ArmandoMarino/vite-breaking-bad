d806d5ca4a96ea0f309a16461e279cb03ceb1baf
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-26 10:28:04 +0100

    default option select

--- src/components/pokemons/DropMenu.vue
+++ src/components/pokemons/DropMenu.vue
@@ -17,8 +17,8 @@ export default {
     <div class="dropdown">
         <label for="pokemons">Choose type:</label>
         <select v-model="selected" name="pokemons" id="pokemons" @click="$emit('selected-change', selected)">
-            <option v-for="pokemontype in pokemontypes" :value="pokemontype">
-                {{ pokemontype }}</option>
+            <option value="">---</option>
+            <option v-for="pokemontype in pokemontypes" :value="pokemontype">{{ pokemontype }}</option>
         </select>
     </div>
 </template>
